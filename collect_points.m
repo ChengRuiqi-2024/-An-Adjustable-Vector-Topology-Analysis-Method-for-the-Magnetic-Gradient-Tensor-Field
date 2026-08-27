85f4291c288334c5afb8f1f0fbf2ae75fc5a57a3
ChengRuiqi-2024 <chengrq24@mails.jlu.edu.cn>
2026-08-27 10:50:38 +0800

    Delete collect_points.m

--- collect_points.m
+++
@@ -1,7 +0,0 @@
-function p = collect_points(mask,X,Y,result)
-    [p.row,p.col] = find(mask);
-    p.x = X(mask);
-    p.y = Y(mask);
-    p.index = result.index(mask);
-    p.score = result.score(mask);
-end
